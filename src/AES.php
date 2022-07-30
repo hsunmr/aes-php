214989c535356393905d11c8f4a7c110414a5b2a
raymond <raymond.chen@25sprout.com>
2022-07-31 01:00:35 +0800

    feat: add AES encrypt and decrypt

---
+++ src/AES.php
@@ -0,0 +1,32 @@
+<?php
+
+class AES
+{
+    /**
+     * encrypt
+     *
+     * @param  string $original_string
+     * @param  string $cipher_algo
+     * @param  string $key
+     * @param  string $iv
+     * @return string
+     */
+    public static function encrypt(string $original_string, string $cipher_algo, string $key, string $iv): string
+    {
+        return openssl_encrypt($original_string, $cipher_algo, $key, 0, $iv);
+    }
+
+    /**
+     * decrypt
+     *
+     * @param  string $encrypted_string
+     * @param  string $cipher_algo
+     * @param  string $key
+     * @param  string $iv
+     * @return string
+     */
+    public static function decrypt(string $encrypted_string, string $cipher_algo, string $key, string $iv): string
+    {
+        return openssl_decrypt($encrypted_string, $cipher_algo, $key, 0, $iv);
+    }
+}
